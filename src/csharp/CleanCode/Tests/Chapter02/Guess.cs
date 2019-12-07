d0bbc141a0c85b98dff3068a22045c89ef76fe3b
Paul Herrera <linus_pagan@hotmail.com>
2019-12-07 06:29:53 -0600

    Non refactored

---
+++ src/csharp/CleanCode/Tests/Chapter02/Guess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tests.Chapter02
+{
+    public class Guess
+    {
+        private readonly Action<string> _printAction;
+
+        public Guess(
+            Action<string> printAction)
+        {
+            _printAction = printAction;
+        }
+
+        public void PrintGuessStatistics(
+            string candidate,
+            int count)
+        {
+            string number;
+            string verb;
+            string pluralModifier;
+            if (count == 0)
+            {
+                number = "no";
+                verb = "are";
+                pluralModifier = "s";
+            }
+            else if (count == 1)
+            {
+                number = "1";
+                verb = "is";
+                pluralModifier = "";
+            }
+            else
+            {
+                number = count.ToString();
+                verb = "are";
+                pluralModifier = "s";
+            }
+
+            string guessMessage = string.Format("There {0} {1} {2}{3}", verb, number, candidate, pluralModifier);
+
+            _printAction(guessMessage);
+        }
+    }
+}
---
+++ src/csharp/CleanCode/Tests/Chapter02/GuessTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Chapter02
+{
+    public class GuessTests
+    {
+        [Fact]
+        public void ShouldReturnExpectedStringGivenZeroCount()
+        {
+            var messages = new List<string>();
+
+            var guess = CreateGuess(messages);
+
+            guess.PrintGuessStatistics("electron", 0);
+
+            AssertSingleMessage(
+                "There are no electrons",
+                messages);
+        }
+
+        [Fact]
+        public void ShouldReturnSingularStringGivenOneCount()
+        {
+            var messages = new List<string>();
+
+            var guess = CreateGuess(messages);
+
+            guess.PrintGuessStatistics("neutrino", 1);
+
+            AssertSingleMessage(
+                "There is 1 neutrino",
+                messages);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+
+        public void ShouldReturnPluralStringGivenCountTwoOrGreater(
+            int itemCount)
+        {
+            var messages = new List<string>();
+
+            var guess = CreateGuess(messages);
+
+            guess.PrintGuessStatistics("quark", itemCount);
+
+            AssertSingleMessage(
+                $"There are {itemCount} quarks",
+                messages);
+        }
+
+        private static Guess CreateGuess(
+            List<string> messages)
+        {
+            return new Guess(message => messages.Add(message));
+        }
+
+        private static void AssertSingleMessage(
+            string expectedMessage,
+            List<string> messages)
+        {
+            messages.Should().HaveCount(1);
+            messages[0].Should().Be(expectedMessage);
+        }
+    }
+}
