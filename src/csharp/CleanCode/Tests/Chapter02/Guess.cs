443fdc5e7f7937f281374fb52a879fc87b8a5a4b
Paul Herrera <linus_pagan@hotmail.com>
2019-12-08 04:53:11 -0600

    Refactored guess

--- src/csharp/CleanCode/Tests/Chapter02/Guess.cs
+++ src/csharp/CleanCode/Tests/Chapter02/Guess.cs
@@ -16,31 +16,58 @@ namespace Tests.Chapter02
             string candidate,
             int count)
         {
-            string number;
-            string verb;
-            string pluralModifier;
-            if (count == 0)
-            {
-                number = "no";
-                verb = "are";
-                pluralModifier = "s";
-            }
-            else if (count == 1)
-            {
-                number = "1";
-                verb = "is";
-                pluralModifier = "";
-            }
-            else
-            {
-                number = count.ToString();
-                verb = "are";
-                pluralModifier = "s";
-            }
-
-            string guessMessage = string.Format("There {0} {1} {2}{3}", verb, number, candidate, pluralModifier);
+            var guessMessage = new GuessMessageCreator()
+                .Create(candidate, count);
 
             _printAction(guessMessage);
+        }
+    }
+
+    public class GuessMessageCreator
+    {
+        private string _number;
+        private string _verb;
+        private string _pluralModifier;
+
+        public string Create(
+            string itemName,
+            int itemCount)
+        {
+            if (itemCount == 0)
+                SetupNoItemsMessage();
+            else if (itemCount == 1)
+                SetupSingleItemMessage();
+            else
+                SetupManyItemsMessage(itemCount);
+
+            return GetMessage(itemName);
+        }
+
+        private string GetMessage(string itemName)
+        {
+            return $"There {_verb} {_number} {itemName}{_pluralModifier}";
+        }
+
+        private void SetupManyItemsMessage(
+            int itemCount)
+        {
+            _number = itemCount.ToString();
+            _verb = "are";
+            _pluralModifier = "s";
+        }
+
+        private void SetupSingleItemMessage()
+        {
+            _number = "1";
+            _verb = "is";
+            _pluralModifier = "";
+        }
+
+        private void SetupNoItemsMessage()
+        {
+            _number = "no";
+            _verb = "are";
+            _pluralModifier = "s";
         }
     }
 }
