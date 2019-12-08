610521fbe456474daa51deaf9d7c26ef858ddaea
Paul Herrera <linus_pagan@hotmail.com>
2019-12-08 07:41:01 -0600

    Added test 1

---
+++ src/csharp/TradeRequestContraint/UnitTests/EncumberedFilterTests.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+using SharedKernel;
+using SharedKernel.Filters;
+using Xunit;
+
+namespace UnitTests
+{
+    public class EncumberedFilterTests
+    {
+        [Fact]
+        public void ShouldNotFilterWhenFilterIsNotActive()
+        {
+            var encumberedFilter = new EncumberedFilter(100);
+
+            var tradeRequest = new TradeRequest()
+            {
+                TradeSide = TradeSide.Sell,
+                HoldingsQuantity = 100,
+                EncumberedQuantity = 30,
+                Stock = new Stock()
+                {
+                }
+            };
+
+            var tradeFilterPreference = new TradeFilterPreference()
+            {
+                IsCapacityEncumberedSharesFilterActive = false
+            };
+
+            encumberedFilter.ApplyFilter(tradeRequest, tradeFilterPreference);
+
+            encumberedFilter.FilteredQuantity.Should().Be(0);
+            encumberedFilter.AvailQuantity.Should().Be(100);
+        }
+
+        [Fact]
+        public void ShouldNotFilterWhenTradeIsBuy()
+        {
+            var encumberedFilter = new EncumberedFilter(100);
+
+            var tradeRequest = new TradeRequest()
+            {
+                TradeSide = TradeSide.Buy,
+                HoldingsQuantity = 100,
+                EncumberedQuantity = 30,
+                Stock = new Stock()
+                {
+                }
+            };
+
+            var tradeFilterPreference = new TradeFilterPreference()
+            {
+                IsCapacityEncumberedSharesFilterActive = true
+            };
+
+            encumberedFilter.ApplyFilter(tradeRequest, tradeFilterPreference);
+
+            encumberedFilter.FilteredQuantity.Should().Be(0);
+            encumberedFilter.AvailQuantity.Should().Be(100);
+        }
+
+        [Fact]
+        public void ShouldFilterWhenTradeIsSell()
+        {
+            var encumberedFilter = new EncumberedFilter(100);
+
+            var tradeRequest = new TradeRequest()
+            {
+                TradeSide = TradeSide.Sell,
+                HoldingsQuantity = 100,
+                EncumberedQuantity = 30,
+                Stock = new Stock()
+                {
+                }
+            };
+
+            var tradeFilterPreference = new TradeFilterPreference()
+            {
+                IsCapacityEncumberedSharesFilterActive = true
+            };
+
+            encumberedFilter.ApplyFilter(tradeRequest, tradeFilterPreference);
+
+            encumberedFilter.FilteredQuantity.Should().Be(30);
+            encumberedFilter.AvailQuantity.Should().Be(70);
+        }
+    }
+}
