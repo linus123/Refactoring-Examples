bbe461146ab2df57812f94a955792daf6591fc7c
Paul Herrera <linus_pagan@hotmail.com>
2019-12-08 08:05:23 -0600

    Added more test

---
+++ src/csharp/TradeRequestContraint/UnitTests/PrimaryLimitFilterTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using SharedKernel;
+using SharedKernel.Filters;
+using Xunit;
+
+namespace UnitTests
+{
+    public class PrimaryLimitFilterTests
+    {
+        [Fact]
+        public void ShouldNotApplyFilterWhenIsPrimaryFilterActiveIsFalse()
+        {
+            var primaryLimitFilter = new PrimaryLimitFilter(200);
+
+            var tradeRequest = new TradeRequest()
+            {
+                Stock = new Stock()
+            };
+
+            var tradeFilterPreference = new TradeFilterPreference()
+            {
+                IsPrimaryFilterActive = false
+            };
+
+            primaryLimitFilter.ApplyFilter(tradeRequest, tradeFilterPreference);
+
+            primaryLimitFilter.FilteredQuantity.Should().Be(0);
+            primaryLimitFilter.AvailQuantity.Should().Be(200);
+        }
+    }
+}
