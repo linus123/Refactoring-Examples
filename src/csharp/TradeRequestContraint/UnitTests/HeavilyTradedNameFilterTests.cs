3ac4d70c357f8e7b69d404eb53f71c190cd47105
Paul Herrera <linus_pagan@hotmail.com>
2019-12-09 04:03:19 -0600

    Refactored HeavilyTradedNameFilterTests 5

--- src/csharp/TradeRequestContraint/UnitTests/HeavilyTradedNameFilterTests.cs
+++ src/csharp/TradeRequestContraint/UnitTests/HeavilyTradedNameFilterTests.cs
@@ -106,7 +106,8 @@ namespace UnitTests
             tradeRequest.Stock.IsHeavilyTradedNameConstraintChecked.Should().BeTrue();
         }
 
-        [Fact] public void ShouldNotApplyFilterShouldBeNoAppliedIsHeavilyTradedNameConstraintCheckedOnStockIsTrue()
+        [Fact]
+        public void ShouldNotApplyFilterShouldBeNoAppliedIsHeavilyTradedNameConstraintCheckedOnStockIsTrue()
         {
             var tradeFilterPreference = new TradeFilterPreferenceBuilder()
                 .WithHeavilyTradedFilterActive(0.9m, 1)
@@ -134,6 +135,50 @@ namespace UnitTests
             tradeRequest.AvailableCapacityQuantity.Should().Be(tradeRequest.OriginalCapacityQuantity);
 
             tradeRequest.Stock.ConstrainedByHeavilyTradedName.Should().BeFalse();
+            tradeRequest.Stock.IsHeavilyTradedNameConstraintChecked.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldApplyFilterShouldBeAppiedConstrainedByHeavilyTradedNameIsTrue()
+        {
+            var tradeFilterPreference = new TradeFilterPreferenceBuilder()
+                .WithHeavilyTradedFilterActive(0.9m, 1)
+                .Create();
+
+            var stock = new StockBuilder()
+                .Create();
+
+            stock.SetVolumes(
+                new decimal[] { 2000, 2100, 2200, 2300, 2400 },
+                new decimal[] { 1000, 1100, 1200, 1300, 1400 });
+
+            stock.IsHeavilyTradedNameConstraintChecked = true;
+            stock.ConstrainedByHeavilyTradedName = true;
+
+            var tradeRequest = new TradeRequestBuilder()
+                .WithStock(stock)
+                .WithTradeFilterPreference(tradeFilterPreference)
+                .Create();
+
+            var tradeRequestCollection = new TradeRequestCollection(new[] { tradeRequest });
+
+            tradeRequestCollection.ApplyFilters();
+
+            tradeRequest.Filters.Should().HaveCount(1);
+            tradeRequest.AvailableCapacityQuantity.Should().Be(0);
+
+            var filter = tradeRequest.Filters[0];
+
+            new FilterAssert(filter)
+                .FilteredQuantityShouldBe(tradeRequest.OriginalCapacityQuantity)
+                .FilterTypeShouldBe("Heavily Traded Name")
+                .OriginalQuantityShouldBe(tradeRequest.OriginalCapacityQuantity)
+                .AvailableQuantityShouldBe(0)
+                .FilteredAmountQuantityShouldBe(tradeRequest.OriginalCapacityQuantity * stock.PriceInUsd)
+                .FilterDescriptionQuantityShouldBe(null)
+                .IsAppliedShouldBeTrue();
+
+            tradeRequest.Stock.ConstrainedByHeavilyTradedName.Should().BeTrue();
             tradeRequest.Stock.IsHeavilyTradedNameConstraintChecked.Should().BeTrue();
         }
     }
