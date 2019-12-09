7bf1ea378b420552ef141814ed2777b086ced4a3
Paul Herrera <linus_pagan@hotmail.com>
2019-12-09 03:30:58 -0600

    Moved class

--- src/csharp/TradeRequestContraint/UnitTests/EncumberedFilterTests.cs
+++ src/csharp/TradeRequestContraint/UnitTests/EncumberedFilterTests.cs
@@ -1,6 +1,5 @@
 ﻿using FluentAssertions;
 using SharedKernel;
-using SharedKernel.Filters;
 using Xunit;
 
 namespace UnitTests
@@ -95,74 +94,6 @@ namespace UnitTests
                 .IsAppliedShouldBeTrue();
 
             tradeRequest.AvailableCapacityQuantity.Should().BeApproximately(170m, precision);
-        }
-
-        private class FilterAssert
-        {
-            private const decimal Precision = 0.00001m;
-
-            private Filter _filter;
-
-            public FilterAssert(
-                Filter filter)
-            {
-                _filter = filter;
-            }
-
-            public FilterAssert FilteredQuantityShouldBe(
-                decimal v)
-            {
-                _filter.FilteredQuantity.Should().BeApproximately(v, Precision);
-
-                return this;
-            }
-
-            public FilterAssert FilterTypeShouldBe(
-                string v)
-            {
-                _filter.FilterType.Should().Be(v);
-
-                return this;
-            }
-
-            public FilterAssert OriginalQuantityShouldBe(
-                decimal v)
-            {
-                _filter.OriginalQuantity.Should().BeApproximately(v, Precision);
-
-                return this;
-            }
-
-            public FilterAssert AvailableQuantityShouldBe(
-                decimal v)
-            {
-                _filter.AvailableQuantity.Should().BeApproximately(v, Precision);
-
-                return this;
-            }
-
-            public FilterAssert FilteredAmountQuantityShouldBe(
-                decimal v)
-            {
-                _filter.FilteredAmount.Should().BeApproximately(v, Precision);
-
-                return this;
-            }
-
-            public FilterAssert FilterDescriptionQuantityShouldBe(
-                string v)
-            {
-                _filter.FilterDescription.Should().Be(v);
-
-                return this;
-            }
-
-            public FilterAssert IsAppliedShouldBeTrue()
-            {
-                _filter.IsApplied.Should().BeTrue();
-
-                return this;
-            }
         }
     }
 }
---
+++ src/csharp/TradeRequestContraint/UnitTests/FilterAssert.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using SharedKernel.Filters;
+
+namespace UnitTests
+{
+    public class FilterAssert
+    {
+        private const decimal Precision = 0.00001m;
+
+        private readonly Filter _filter;
+
+        public FilterAssert(
+            Filter filter)
+        {
+            _filter = filter;
+        }
+
+        public FilterAssert FilteredQuantityShouldBe(
+            decimal v)
+        {
+            _filter.FilteredQuantity.Should().BeApproximately(v, Precision);
+
+            return this;
+        }
+
+        public FilterAssert FilterTypeShouldBe(
+            string v)
+        {
+            _filter.FilterType.Should().Be(v);
+
+            return this;
+        }
+
+        public FilterAssert OriginalQuantityShouldBe(
+            decimal v)
+        {
+            _filter.OriginalQuantity.Should().BeApproximately(v, Precision);
+
+            return this;
+        }
+
+        public FilterAssert AvailableQuantityShouldBe(
+            decimal v)
+        {
+            _filter.AvailableQuantity.Should().BeApproximately(v, Precision);
+
+            return this;
+        }
+
+        public FilterAssert FilteredAmountQuantityShouldBe(
+            decimal v)
+        {
+            _filter.FilteredAmount.Should().BeApproximately(v, Precision);
+
+            return this;
+        }
+
+        public FilterAssert FilterDescriptionQuantityShouldBe(
+            string v)
+        {
+            _filter.FilterDescription.Should().Be(v);
+
+            return this;
+        }
+
+        public FilterAssert IsAppliedShouldBeTrue()
+        {
+            _filter.IsApplied.Should().BeTrue();
+
+            return this;
+        }
+    }
+}
