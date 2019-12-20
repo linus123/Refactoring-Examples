f703c888dd26bfdf9d3e147623ea70329703e64c
Paul Herrera <paul.herrera@dimensional.com>
2019-12-20 10:14:53 -0600

    Added one off project

---
+++ src/csharp/SimpleCodeExample/UnitTests/CleanCodeComment/GeneratePrimesTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using ProductionCode.CleanCodeComment;
+using Xunit;
+
+namespace UnitTests.CleanCodeComment
+{
+    public class GeneratePrimesTests
+    {
+        [Fact]
+        public void ShouldWork()
+        {
+            var generate = GeneratePrimes.Generate(20);
+
+            generate.Length.Should().Be(8);
+
+            generate.Should().Equal(2, 3, 5, 7, 11, 13, 17, 19);
+        }
+    }
+}
