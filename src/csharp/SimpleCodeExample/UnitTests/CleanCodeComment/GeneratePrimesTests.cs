c1684804b4d60aa7ea10db5f129b1225a9622edf
Paul Herrera <paul.herrera@dimensional.com>
2019-12-20 13:01:58 -0600

    Completed test

--- src/csharp/SimpleCodeExample/UnitTests/CleanCodeComment/GeneratePrimesTests.cs
+++ src/csharp/SimpleCodeExample/UnitTests/CleanCodeComment/GeneratePrimesTests.cs
@@ -7,18 +7,20 @@ namespace UnitTests.CleanCodeComment
 {
     public class GeneratePrimesTests
     {
-        public static IEnumerable<object[]> GetData(int numTests)
+        public static IEnumerable<object[]> GetMaxValueAndExpected(
+            int numTests)
         {
             yield return new object[] {1, new int[0]};
             yield return new object[] {2, new [] { 2 }};
             yield return new object[] {3, new [] { 2, 3 }};
             yield return new object[] {4, new [] { 2, 3 }};
             yield return new object[] {5, new [] { 2, 3, 5 }};
+            yield return new object[] {20, new [] { 2, 3, 5, 7, 11, 13, 17, 19 }};
         }
 
         [Theory]
-        [MemberData(nameof(GetData), parameters: 2)]
-        public void ShouldWork(
+        [MemberData(nameof(GetMaxValueAndExpected), parameters: 2)]
+        public void ShouldReturnExpectedValues(
             int maxValue,
             int[] expected)
         {
