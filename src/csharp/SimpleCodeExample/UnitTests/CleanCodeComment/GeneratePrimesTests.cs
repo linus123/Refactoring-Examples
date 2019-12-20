c78354fb5fe1cd7f5b549f29a11263602fe2d74b
Paul Herrera <paul.herrera@dimensional.com>
2019-12-20 13:00:28 -0600

    Added tests for code

--- src/csharp/SimpleCodeExample/UnitTests/CleanCodeComment/GeneratePrimesTests.cs
+++ src/csharp/SimpleCodeExample/UnitTests/CleanCodeComment/GeneratePrimesTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using ProductionCode.CleanCodeComment;
 using Xunit;
 
@@ -6,14 +7,23 @@ namespace UnitTests.CleanCodeComment
 {
     public class GeneratePrimesTests
     {
-        [Fact]
-        public void ShouldWork()
+        public static IEnumerable<object[]> GetData(int numTests)
         {
-            var generate = GeneratePrimes.Generate(20);
+            yield return new object[] {1, new int[0]};
+            yield return new object[] {2, new [] { 2 }};
+            yield return new object[] {3, new [] { 2, 3 }};
+            yield return new object[] {4, new [] { 2, 3 }};
+            yield return new object[] {5, new [] { 2, 3, 5 }};
+        }
 
-            generate.Length.Should().Be(8);
-
-            generate.Should().Equal(2, 3, 5, 7, 11, 13, 17, 19);
+        [Theory]
+        [MemberData(nameof(GetData), parameters: 2)]
+        public void ShouldWork(
+            int maxValue,
+            int[] expected)
+        {
+            GeneratePrimes.Generate(maxValue)
+                .Should().Equal(expected);
         }
     }
 }
