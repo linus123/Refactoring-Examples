291692bda2cf01706067d88495227f2e99a8a9a8
Paul Herrera <linus_pagan@hotmail.com>
2019-12-26 05:15:38 -0600

    Added repository

---
+++ src/csharp/SqlExample/ProductionCode/TradeVolume/TradeHistoryRepository.cs
@@ -0,0 +1,78 @@
+﻿using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace ProductionCode.TradeVolume
+{
+    public class TradeHistoryRepository
+    {
+        private readonly string _connectionString;
+
+        public TradeHistoryRepository(
+            string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public TradeVolumeHistory[] GetTradeVolumeHistories()
+        {
+            TradeVolumeHistory[] tradeVolumeHistories = null;
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                tradeVolumeHistories = connection
+                    .Query<TradeVolumeHistory>(TradeVolumesQuery)
+                    .ToArray();
+
+                connection.Close();
+            }
+
+            return tradeVolumeHistories;
+        }
+
+        private const string TradeVolumesQuery = @"
+            CREATE TABLE #CusipSedol
+            (
+                CusipSedol VARCHAR(30) NOT NULL,
+                PRIMARY KEY(CusipSedol)
+            )
+
+            INSERT INTO #CusipSedol 
+            SELECT pairs.FirstValue as 'CusipSedol' 
+            FROM dbo.udf_ParseValuePairsToTable(@cusipSedol,',',';') as pairs
+
+            SELECT CusipSedol, 
+                BrokerCode, 
+                ISNULL([1],0) AS Day1,
+                ISNULL([2],0) AS Day2,
+                ISNULL([3],0) AS Day3,
+                ISNULL([4],0) AS Day4,
+                ISNULL([5],0) AS Day5,
+                ISNULL([6],0) AS Day6,
+                ISNULL([7],0) AS Day7,
+                ISNULL([8],0) AS Day8,
+                ISNULL([9],0) AS Day9,
+                ISNULL([10],0) AS Day10
+            FROM
+            (
+                SELECT Cusip_Sedol AS 'CusipSedol', Broker_Code AS 'BrokerCode',ABS(Trade_Shares) AS 'Trade_Shares',
+                DATEDIFF(dd, Trade_Date, @tradeDate) 
+                    + CASE WHEN DATEPART(dw,  @tradeDate) = 7 THEN 1 ELSE 0 END  
+                       - (DATEDIFF(wk,  Trade_Date, @tradeDate) * 2 ) 
+                       - CASE WHEN DATEPART(dw,  @tradeDate) = 1 THEN 1 ELSE 0 END  
+                       - CASE WHEN DATEPART(dw,  @tradeDate) = 1 THEN 1 ELSE 0 
+                       END AS 'Day'   
+                FROM #CusipSedol s JOIN dbo.tblTrading_Trades t ON s.CusipSedol = t.Cusip_Sedol
+            ) p
+            PIVOT(
+                SUM(Trade_Shares)
+                FOR Day IN
+                (
+                    [1],[2],[3],[4],[5],[6],[7],[8],[9],[10]
+                )
+            ) AS pvt
+        ";
+    }
+}
---
+++ src/csharp/SqlExample/ProductionCode/TradeVolume/TradeVolumeHistory.cs
@@ -0,0 +1,91 @@
+﻿namespace ProductionCode.TradeVolume
+{
+    public class TradeVolumeHistory
+    {
+        private int length = 10;
+
+        public string CusipSedol { get; set; }
+        public string BrokerCode { get; set; }
+        public decimal Day1 { get; set; }
+        public decimal Day2 { get; set; }
+        public decimal Day3 { get; set; }
+        public decimal Day4 { get; set; }
+        public decimal Day5 { get; set; }
+        public decimal Day6 { get; set; }
+        public decimal Day7 { get; set; }
+        public decimal Day8 { get; set; }
+        public decimal Day9 { get; set; }
+        public decimal Day10 { get; set; }
+
+        private decimal[] Accumulated10DayVolume;
+
+        public void Accumulate10DayVolume()
+        {
+            Accumulated10DayVolume = new decimal[length];
+            for (int i = 0; i < length; i++)
+            {
+                Accumulated10DayVolume[i] = GetDayVolume(i + 1);
+            }
+            for (int i = 1; i < length; i++)
+            {
+                Accumulated10DayVolume[i] += Accumulated10DayVolume[i - 1];
+            }
+        }
+
+        public decimal GetAccumulatedDayVolume(int dayNo)
+        {
+            decimal accumulatedVolume = 0;
+            if (Accumulated10DayVolume != null)
+            {
+                dayNo -= 1;
+                if (dayNo >= 0 && dayNo < Accumulated10DayVolume.Length)
+                {
+                    accumulatedVolume = Accumulated10DayVolume[dayNo];
+                }
+            }
+            return accumulatedVolume;
+        }
+
+        private decimal GetDayVolume(int dayNo)
+        {
+            decimal dayVolume = 0;
+            switch (dayNo)
+            {
+                case 1:
+                    dayVolume = Day1;
+                    break;
+                case 2:
+                    dayVolume = Day2;
+                    break;
+                case 3:
+                    dayVolume = Day3;
+                    break;
+                case 4:
+                    dayVolume = Day4;
+                    break;
+                case 5:
+                    dayVolume = Day5;
+                    break;
+                case 6:
+                    dayVolume = Day6;
+                    break;
+                case 7:
+                    dayVolume = Day7;
+                    break;
+                case 8:
+                    dayVolume = Day8;
+                    break;
+                case 9:
+                    dayVolume = Day9;
+                    break;
+                case 10:
+                    dayVolume = Day10;
+                    break;
+                default:
+                    break;
+            }
+            return dayVolume;
+        }
+    }
+
+}
