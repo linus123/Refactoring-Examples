6b1d1eb4935303ca20ea4e0995447f22998646bc
Paul Herrera <pherrera@dimensional.com>
2020-01-09 13:10:25 -0600

    Made progress on money example

---
+++ src/csharp/SimpleExample/ProductionCode/TestDrivenDev/Money.cs
@@ -0,0 +1,59 @@
+﻿namespace ProductionCode.TestDrivenDev
+{
+    public abstract class Money
+    {
+        protected int Amount;
+
+        public static Money Dollar(int amount)
+        {
+            return new Dollar(amount);
+        }
+
+        public static Money Franc(int amount)
+        {
+            return new Franc(amount);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var money = (Money)obj;
+
+            return Amount == money.Amount
+                && GetType() == obj.GetType();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public abstract Money Times(int multiplier);
+    }
+
+    public class Dollar : Money
+    {
+        public Dollar(int amount)
+        {
+            Amount = amount;
+        }
+
+        public override Money Times(int multiplier)
+        {
+            return new Dollar(Amount * multiplier);
+        }
+    }
+
+    public class Franc : Money
+    {
+        public Franc(int amount)
+        {
+            Amount = amount;
+        }
+
+        public override Money Times(int multiplier)
+        {
+            return new Franc(Amount * multiplier);
+        }
+    }
+
+}
---
+++ src/csharp/SimpleExample/UnitTests/TestDrivenDev/MoneyTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using ProductionCode.TestDrivenDev;
+using Xunit;
+
+namespace TestCode.TestDrivenDev
+{
+    public class MoneyTests
+    {
+        [Fact]
+        public void DollarMultiplication()
+        {
+            Money five = Money.Dollar(5);
+            five.Times(2).Should().Be(Money.Dollar(10));
+            five.Times(3).Should().Be(Money.Dollar(15));
+        }
+
+        [Fact]
+        public void EqualityDollar()
+        {
+            var five = Money.Dollar(5);
+
+            five.Equals(Money.Dollar(5)).Should().BeTrue();
+            five.Equals(Money.Dollar(6)).Should().BeFalse();
+        }
+
+        [Fact]
+        public void FrancMultiplication()
+        {
+            var five = Money.Franc(5);
+
+            five.Times(2).Should().Be(Money.Franc(10));
+            five.Times(3).Should().Be(Money.Franc(15));
+        }
+
+        [Fact]
+        public void EqualityFranc()
+        {
+            var five = Money.Franc(5);
+
+            five.Equals(Money.Franc(5)).Should().BeTrue();
+            five.Equals(Money.Franc(6)).Should().BeFalse();
+        }
+
+        [Fact]
+        public void EqualityDollarAndFrank()
+        {
+            Money.Franc(5).Should().NotBe(Money.Dollar(5));
+        }
+
+    }
+}
