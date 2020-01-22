7f02684135d0a2f0c710bb1b431ee265c83dd390
Paul Herrera <paul.herrera@dimensional.com>
2020-01-22 04:53:00 -0600

    Separated class and converted to member variables

--- src/csharp/SimpleExample/ProductionCode/PrintPrime/PrimePrinterHelper.cs
+++ src/csharp/SimpleExample/ProductionCode/PrintPrime/PrimePrinterHelper.cs
@@ -4,74 +4,77 @@ namespace ProductionCode.PrintPrime
 {
     public class PrimePrinterHelper
     {
+        private int[] _primes;
+        private int _pageNumber;
+        private int _linesPerPage;
+        private int _rowOffset;
+        private int _column;
+        private int _candidate;
+        private int _primeIndex;
+        private bool _possiblyPrime;
+        private int _index0;
+        private int _square;
+        private int _i;
+        private int[] _multiples;
+        private const int NumberOfPrimes = 1000;
+        private const int LinesPerPage = 50;
+        private const int Columns = 4;
+        private const int Ordmax = 30;
+
         public void Invoke()
         {
-            const int numberOfPrimes = 1000;
-            const int linesPerPage = 50;
-            const int columns = 4;
-            const int ordmax = 30;
-            int[] primes = new int[numberOfPrimes + 1];
-            int pageNumber;
-            int pageOffset;
-            int rowOffset;
-            int column;
-            int candidate;
-            int primeIndex;
-            bool possiblyPrime;
-            int ord;
-            int square;
-            int n;
-            int[] multiples = new int[ordmax + 1];
+            _primes = new int[NumberOfPrimes + 1];
+            _multiples = new int[Ordmax + 1];
 
-            candidate = 1;
-            primeIndex = 1;
-            primes[1] = 2;
-            ord = 2;
-            square = 9;
+            _candidate = 1;
+            _primeIndex = 1;
+            _primes[1] = 2;
+            _index0 = 2;
+            _square = 9;
 
-            while (primeIndex < numberOfPrimes)
+            while (_primeIndex < NumberOfPrimes)
             {
                 do
                 {
-                    candidate = candidate + 2;
-                    if (candidate == square)
+                    _candidate = _candidate + 2;
+                    if (_candidate == _square)
                     {
-                        ord = ord + 1;
-                        square = primes[ord] * primes[ord];
-                        multiples[ord - 1] = candidate;
+                        _index0 = _index0 + 1;
+                        _square = _primes[_index0] * _primes[_index0];
+                        _multiples[_index0 - 1] = _candidate;
                     }
-                    n = 2;
-                    possiblyPrime = true;
-                    while (n < ord && possiblyPrime)
+                    _i = 2;
+                    _possiblyPrime = true;
+                    while (_i < _index0 && _possiblyPrime)
                     {
-                        while (multiples[n] < candidate)
-                            multiples[n] = multiples[n] + primes[n] + primes[n];
-                        if (multiples[n] == candidate)
-                            possiblyPrime = false;
-                        n = n + 1;
+                        while (_multiples[_i] < _candidate)
+                            _multiples[_i] = _multiples[_i] + _primes[_i] + _primes[_i];
+                        if (_multiples[_i] == _candidate)
+                            _possiblyPrime = false;
+                        _i = _i + 1;
                     }
-                } while (!possiblyPrime);
-                primeIndex = primeIndex + 1;
-                primes[primeIndex] = candidate;
+                } while (!_possiblyPrime);
+                _primeIndex = _primeIndex + 1;
+                _primes[_primeIndex] = _candidate;
             }
             {
-                pageNumber = 1;
-                pageOffset = 1;
-                while (pageOffset <= numberOfPrimes)
+                _pageNumber = 1;
+                _linesPerPage = 1;
+                while (_linesPerPage <= NumberOfPrimes)
                 {
-                    Console.WriteLine("The First " + numberOfPrimes +
-                                       " Prime Numbers --- Page " + pageNumber);
+                    Console.WriteLine("The First " + NumberOfPrimes +
+                                       " Prime Numbers --- Page " + _pageNumber);
                     Console.WriteLine("");
-                    for (rowOffset = pageOffset; rowOffset < pageOffset + linesPerPage; rowOffset++)
+                    for (_rowOffset = _linesPerPage; _rowOffset < _linesPerPage + LinesPerPage; _rowOffset++)
                     {
-                        for (column = 0; column < columns; column++)
-                            if (rowOffset + column * linesPerPage <= numberOfPrimes)
-                                Console.Write("{0, 10}", primes[rowOffset + column * linesPerPage]);
+                        for (_column = 0; _column < Columns; _column++)
+                            if (_rowOffset + _column * LinesPerPage <= NumberOfPrimes)
+                                Console.Write("{0, 10}", _primes[_rowOffset + _column * LinesPerPage]);
                         Console.WriteLine("");
                     }
                     Console.WriteLine();
-                    pageNumber = pageNumber + 1;
-                    pageOffset = pageOffset + linesPerPage * columns;
+                    _pageNumber = _pageNumber + 1;
+                    _linesPerPage = _linesPerPage + LinesPerPage * Columns;
                 }
             }
         }
