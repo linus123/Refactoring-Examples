dbcfbc099e9cac37058d05a24fd456c05599d83c
Paul Herrera <paul.herrera@dimensional.com>
2020-01-22 04:46:38 -0600

    Corrected spelling

--- src/csharp/SimpleExample/ProductionCode/PrintPrime/PrintPrimes.cs
+++ src/csharp/SimpleExample/ProductionCode/PrintPrime/PrintPrimes.cs
@@ -15,7 +15,7 @@ namespace ProductionCode.PrintPrime
             int pageOffset;
             int rowOffset;
             int column;
-            int candiate;
+            int candidate;
             int primeIndex;
             bool possiblyPrime;
             int ord;
@@ -23,7 +23,7 @@ namespace ProductionCode.PrintPrime
             int n;
             int[] multiples = new int[ordmax + 1];
 
-            candiate = 1;
+            candidate = 1;
             primeIndex = 1;
             primes[1] = 2;
             ord = 2;
@@ -33,26 +33,26 @@ namespace ProductionCode.PrintPrime
             {
                 do
                 {
-                    candiate = candiate + 2;
-                    if (candiate == square)
+                    candidate = candidate + 2;
+                    if (candidate == square)
                     {
                         ord = ord + 1;
                         square = primes[ord] * primes[ord];
-                        multiples[ord - 1] = candiate;
+                        multiples[ord - 1] = candidate;
                     }
                     n = 2;
                     possiblyPrime = true;
                     while (n < ord && possiblyPrime)
                     {
-                        while (multiples[n] < candiate)
+                        while (multiples[n] < candidate)
                             multiples[n] = multiples[n] + primes[n] + primes[n];
-                        if (multiples[n] == candiate)
+                        if (multiples[n] == candidate)
                             possiblyPrime = false;
                         n = n + 1;
                     }
                 } while (!possiblyPrime);
                 primeIndex = primeIndex + 1;
-                primes[primeIndex] = candiate;
+                primes[primeIndex] = candidate;
             }
             {
                 pageNumber = 1;
