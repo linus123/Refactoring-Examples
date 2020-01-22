192bc04ee8bb770a3bcc7cb7c61b6ebcbd7b4f55
Paul Herrera <paul.herrera@dimensional.com>
2020-01-22 04:30:18 -0600

    Renamed variables

--- src/csharp/SimpleExample/ProductionCode/PrintPrime/PrintPrimes.cs
+++ src/csharp/SimpleExample/ProductionCode/PrintPrime/PrintPrimes.cs
@@ -6,73 +6,72 @@ namespace ProductionCode.PrintPrime
     {
         public void Run()
         {
-            const int M = 1000;
-            const int RR = 50;
-            const int CC = 4;
-            const int WW = 10;
-            const int ORDMAX = 30;
-            int[] P = new int[M + 1];
-            int PAGENUMBER;
-            int PAGEOFFSET;
-            int ROWOFFSET;
-            int C;
-            int J;
-            int K;
-            bool JPRIME;
-            int ORD;
-            int SQUARE;
-            int N;
-            int[] MULT = new int[ORDMAX + 1];
+            const int numberOfPrimes = 1000;
+            const int linesPerPage = 50;
+            const int columns = 4;
+            const int ordmax = 30;
+            int[] primes = new int[numberOfPrimes + 1];
+            int pageNumber;
+            int pageOffset;
+            int rowOffset;
+            int column;
+            int candiate;
+            int primeIndex;
+            bool possiblyPrime;
+            int ord;
+            int square;
+            int n;
+            int[] multiples = new int[ordmax + 1];
 
-            J = 1;
-            K = 1;
-            P[1] = 2;
-            ORD = 2;
-            SQUARE = 9;
+            candiate = 1;
+            primeIndex = 1;
+            primes[1] = 2;
+            ord = 2;
+            square = 9;
 
-            while (K < M)
+            while (primeIndex < numberOfPrimes)
             {
                 do
                 {
-                    J = J + 2;
-                    if (J == SQUARE)
+                    candiate = candiate + 2;
+                    if (candiate == square)
                     {
-                        ORD = ORD + 1;
-                        SQUARE = P[ORD] * P[ORD];
-                        MULT[ORD - 1] = J;
+                        ord = ord + 1;
+                        square = primes[ord] * primes[ord];
+                        multiples[ord - 1] = candiate;
                     }
-                    N = 2;
-                    JPRIME = true;
-                    while (N < ORD && JPRIME)
+                    n = 2;
+                    possiblyPrime = true;
+                    while (n < ord && possiblyPrime)
                     {
-                        while (MULT[N] < J)
-                            MULT[N] = MULT[N] + P[N] + P[N];
-                        if (MULT[N] == J)
-                            JPRIME = false;
-                        N = N + 1;
+                        while (multiples[n] < candiate)
+                            multiples[n] = multiples[n] + primes[n] + primes[n];
+                        if (multiples[n] == candiate)
+                            possiblyPrime = false;
+                        n = n + 1;
                     }
-                } while (!JPRIME);
-                K = K + 1;
-                P[K] = J;
+                } while (!possiblyPrime);
+                primeIndex = primeIndex + 1;
+                primes[primeIndex] = candiate;
             }
             {
-                PAGENUMBER = 1;
-                PAGEOFFSET = 1;
-                while (PAGEOFFSET <= M)
+                pageNumber = 1;
+                pageOffset = 1;
+                while (pageOffset <= numberOfPrimes)
                 {
-                    Console.WriteLine("The First " + M +
-                                       " Prime Numbers --- Page " + PAGENUMBER);
+                    Console.WriteLine("The First " + numberOfPrimes +
+                                       " Prime Numbers --- Page " + pageNumber);
                     Console.WriteLine("");
-                    for (ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++)
+                    for (rowOffset = pageOffset; rowOffset < pageOffset + linesPerPage; rowOffset++)
                     {
-                        for (C = 0; C < CC; C++)
-                            if (ROWOFFSET + C * RR <= M)
-                                Console.Write("{0, 10}", P[ROWOFFSET + C * RR]);
+                        for (column = 0; column < columns; column++)
+                            if (rowOffset + column * linesPerPage <= numberOfPrimes)
+                                Console.Write("{0, 10}", primes[rowOffset + column * linesPerPage]);
                         Console.WriteLine("");
                     }
                     Console.WriteLine();
-                    PAGENUMBER = PAGENUMBER + 1;
-                    PAGEOFFSET = PAGEOFFSET + RR * CC;
+                    pageNumber = pageNumber + 1;
+                    pageOffset = pageOffset + linesPerPage * columns;
                 }
             }
         }
